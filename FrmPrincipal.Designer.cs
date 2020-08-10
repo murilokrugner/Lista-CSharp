32004b31db97413ef373b27aa0f01f729df1251c
murilokrugner <murilokrugner@hotmail.com>
2020-08-10 20:54:19 -0300

    first commit

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LISTA___MURILO_AUGUSTO_KRUGNER_ALMEIDA_LEITE
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnQnt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(35, 168);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(113, 55);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "ADICIONAR";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(136, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(456, 35);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(136, 115);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(456, 26);
+            this.textBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LISTA: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "VALOR: ";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(169, 168);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(113, 55);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "REMOVER";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnQnt
+            // 
+            this.btnQnt.Location = new System.Drawing.Point(315, 168);
+            this.btnQnt.Name = "btnQnt";
+            this.btnQnt.Size = new System.Drawing.Size(153, 55);
+            this.btnQnt.TabIndex = 6;
+            this.btnQnt.Text = "QUANTIDADE";
+            this.btnQnt.UseVisualStyleBackColor = true;
+            this.btnQnt.Click += new System.EventHandler(this.btnQnt_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 235);
+            this.Controls.Add(this.btnQnt);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "FrmPrincipal";
+            this.Text = "LISTA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnQnt;
+    }
+}
+
---
+++ FrmPrincipal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LISTA___MURILO_AUGUSTO_KRUGNER_ALMEIDA_LEITE
+{
+    public partial class FrmPrincipal : Form
+    {
+        List<string> list = new List<string>();
+        Array pointer;
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show("Por favor, digite um valor para adicionar a lista");
+                return;
+            }
+
+            list.Add(textBox.Text);
+
+            pointer = list.ToArray();
+
+            listView1.Items.Clear();
+
+            foreach (string i in pointer)
+            {
+                listView1.Items.Add(i.ToString());
+
+            }
+
+            textBox.Clear();
+
+            textBox.Focus();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "")
+            {
+                list.RemoveAt(0);
+            } else
+            {
+                list.Remove(textBox.Text);
+            }
+
+            pointer = list.ToArray();
+
+            listView1.Items.Clear();
+
+            foreach (string i in pointer)
+            {
+                listView1.Items.Add(i.ToString());
+
+            }
+
+            textBox.Clear();
+
+            textBox.Focus();
+        }
+
+        private void btnQnt_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Quantidade de items adicionados a lista: " + list.Count.ToString());
+
+            textBox.Clear();
+
+            textBox.Focus();
+        }
+    }
+}
